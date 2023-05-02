3864de1f513f21ba02b4d1872a58ba40ec970740
Cristian, ban4 <66863054+CristianJG18@users.noreply.github.com>
2023-05-02 03:26:43 -0300

    Create entrypoin.sh

---
+++ entrypoin.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Give everything time to initialize for preventing SteamCMD deadlock
+sleep 1
+
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-UTC}
+export TZ
+
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Switch to the container's working directory
+cd /home/container || exit 1
+
+# Download and extract cs16.zip from point.netpika.com
+curl -L -o cs16.zip http://point.netpika.com/downloads/cs16.zip
+unzip cs16.zip
+chmod +x hlds_run
+
+# Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
+# variable format of "${VARIABLE}" before evaluating the string and automatically
+# replacing the values.
+PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+
+# Display the command we're running in the output, and then execute it with the env
+# from the container itself.
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
+# shellcheck disable=SC2086
+exec env ${PARSED}"
